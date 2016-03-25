5dd7fbf6deb2ea9a0410f5fdc7237cad5636b023
niki <niki@90zl.com>
2016-03-26 02:36:23 +0800

    add event functions

--- src/mango.js
+++ src/mango.js
@@ -1,16 +1,62 @@
-;(function(window) {
+;
+(function(window) {
 
-    var Mango = {};
-    Mango.addEvent = function(obj, event, callback) {
-        try {
-            obj.addEventListener(event, callback, false);
-        } catch (e) {
-            try {
-                obj.attachEvent('on' + event, callback);
-            } catch (e) {
-                obj['on' + event] = callback;
+    var Mango = {
+
+        //绑定事件
+        addHandler: function(element, type, handler) {
+            if (element.addEventListener) {
+                element.addEventListener(type, handler, false);
+            } else if (element.attachEvent) {
+                element.attachEvent("on" + type, handler);
+            } else {
+                element["on" + type] = handler;
+            }
+        },
+        //移除事件
+        removeHandler: function(element, type, handler) {
+            if (element.removeEventListener) {
+                element.removeEventListener(type, handler, false);
+            } else if (element.detachEvent) {
+                element.detachEvent("on" + type, handler);
+            } else {
+                element["on" + type] = null;
+            }
+        },
+        //获取事件
+        getEvent: function(event) {
+            return event ? event : window.event;
+        },
+        //获取操作对象
+        getTarget: function(event) {
+            return event.target || event.srcElement;
+        },
+        //阻止默认行为
+        preventDefault: function(event) {
+            if (event.preventDefault) {
+                event.preventDefault();
+            } else {
+                event.returnValue = false;
+            }
+        },
+        //阻止事件冒泡
+        stopPropagation: function(event) {
+            if (event.stopPropagation) {
+                event.stopPropagation();
+            } else {
+                event.cancelBubble = true;
+            }
+        },
+        //获取字符编码
+        getCharCode : function(event){
+            if(typeof event.charCode === 'number'){
+                return event.charCode;
+            }else{
+                return event.keyCode;
             }
         }
     };
+
+
     window.mango = window.$ = Mango;
 })(window);
