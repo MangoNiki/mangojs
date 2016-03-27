6fb93b382e4fa0e02e6c7d8b99fff1d54e5a410c
niki <niki@90zl.com>
2016-03-27 19:50:45 +0800

    modify test files

--- requireApp.js
+++ requireApp.js
@@ -1,10 +1,10 @@
 require.config({
-    urlArgs: "bust=" + (new Date()).getTime(),
+    urlArgs: "t=" + (new Date()).getTime(),
     paths: {
         "mango": "dist/mango",
         "index":"test/requireIndex"
     }　　
 });
 require(['mango','index'], function(mango,index) {
-	
+
 });
